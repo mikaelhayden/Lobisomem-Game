a7b504c8d282a5d670148b0d8630a84076634775
Mikael Hayden <mikaelhaydencf@gmail.com>
2024-03-17 19:20:18 -0300

    Feat: :+1:  choose
    I put it so that when the player is born he will be directed to a location and a werewolf will be chosen.

--- src/server/Functions.lua
+++ src/server/Functions.lua
@@ -1,8 +1,22 @@
+--local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
 local module = {}
 
 function module.ChooseLobisomem()
     local lobisomem = game.Players:GetPlayers() [math.random(1, #game.Players:GetPlayers())]
 
+    local sword = game.ReplicatedStorage.ClassicSword
+    sword:Clone().Parent = lobisomem.Backpack
+
+end
+
+function module.TPplayers()
+    for i, v in pairs(game.Players:GetPlayers()) do
+        local character = v.Character
+        if character then
+            character.HumanoidRootPart.CFrame = workspace.Map.TPpart.CFrame
+        end
+    end
 end
 
 return module
--- src/server/MainScript.server.lua
+++ src/server/MainScript.server.lua
@@ -1,3 +1,26 @@
-local Module =  require(game.ServerScriptService.Server.DayNight)
+--local Module =  require(game.ServerScriptService.Server.DayNight)--
+local module =  require(script:WaitForChild('Functions'))
 
-Module.CicleDay()
+--Module.CicleDay()--------
+
+function startround()
+	module.ChooseLobisomem()
+	wait()
+	module.TPplayers()
+end
+
+while wait() do
+	wait()
+	repeat wait(.5) until game.Players.NumPlayers >=4
+	for i = 10,0,-1 do
+		print(i)
+		wait(i)
+	end
+
+	startround()
+
+	for i = 20, 0, -1 do 
+		print(i)
+		wait(i)
+	end
+end
